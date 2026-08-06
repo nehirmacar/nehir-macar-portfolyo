c5ff9df93b51cbb868ccbccbf1ff4e9e3a86fd2c
nehirmacar <ymacar@gmail.com>
2026-08-06 13:32:07 +0300

    Miyav sesinin seviyesini 0.1'e düşür

--- js/chatbot.js
+++ js/chatbot.js
@@ -19,7 +19,7 @@
   function playMeow() {
     try {
       const audio = new Audio("assets/meow.wav");
-      audio.volume = 0.25;
+      audio.volume = 0.1;
       audio.play().catch(() => {});
     } catch (err) {}
   }
